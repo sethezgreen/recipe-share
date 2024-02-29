0b88d39ed2135110e4a3db3930ceaf5c1a22c426
Seth Green <139513813+sethezgreen@users.noreply.github.com>
2024-02-29 08:56:01 -0500

    Delete flask-server/flask_api/__init__.py

--- flask-server/flask_api/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-api = Flask(__name__)
-CORS(api, supports_credentials=True)
-
-api.secret_key = "The first rule of secret key is... keep your recipes safe" 
