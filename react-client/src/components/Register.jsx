6f21e37440fd3be7c178e52fb0c0fcdc5c2ab6be
Seth Green <sethezgreen@gmail.com>
2024-03-04 09:29:43 -0500

    added error messages to register component

--- react-client/src/components/Register.jsx
+++ react-client/src/components/Register.jsx
@@ -34,40 +34,74 @@ const Register = (props) => {
             .catch((err) => {
                 console.log(err)
                 setErrors(err.response.data)
+                // errors.email = [err.response.data.emailLength, err.response.data.emailValid, err.response.data.emailTaken]
             })
     }
     
     return (
         <div>
+            <p>{errors.email}</p>
             <form onSubmit={submitHandler}>
                 <div>
                     <label>Username:</label>
                     <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    {
+                        errors.usernameLength || errors.usernameTaken?
+                        <div>
+                            <p>{errors.usernameLength}</p>
+                            <p>{errors.usernameTaken}</p>
+                        </div>:
+                        null
+                    }
                 </div>
                 <div>
                     <label>First Name:</label>
                     <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                    {
+                        errors.firstName?
+                        <p>{errors.firstName}</p>:
+                        null
+                    }
                 </div>
                 <div>
                     <label>Last Name:</label>
                     <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                    {
+                        errors.lastName?
+                        <p>{errors.lastName}</p>:
+                        null
+                    }
                 </div>
                 <div>
                     <label>Email:</label>
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     {
-                        errors.email?
-                        <p>{errors.email}</p>:
+                        errors.emailLength || errors.emailValid || errors.emailTaken?
+                        <div>
+                            <p>{errors.emailLength}</p>
+                            <p>{errors.emailValid}</p>
+                            <p>{errors.emailTaken}</p>
+                        </div>:
                         null
                     }
                 </div>
                 <div>
                     <label>Password:</label>
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    {
+                        errors.password?
+                        <p>{errors.password}</p>:
+                        null
+                    }
                 </div>
                 <div>
                     <label>Confirm Password:</label>
                     <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                    {
+                        errors.confirmPassword?
+                        <p>{errors.confirmPassword}</p>:
+                        null
+                    }
                 </div>
                 <button>Register</button>
             </form>
