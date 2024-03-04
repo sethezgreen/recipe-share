02bb0354492df7cae0eadec7d196a34a949ab604
sethezgreen <sethezgreen@gmail.com>
2024-03-03 19:39:04 -0500

    added userId and errors

--- react-client/src/components/RecipeForm.jsx
+++ react-client/src/components/RecipeForm.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-const RecipeForm = () => {
+const RecipeForm = (props) => {
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
     const [ingredients, setIngredients] = useState("")
     const [directions, setDirections] = useState("")
     const [prepTime, setPrepTime] = useState("")
     const [cookTime, setCookTime] = useState("")
-    const [userId, setUserId] = useState(1) // hard coded user id, need to store id in state upon register/logging in
+    const {userId} = props
     const [errors, setErrors] = useState([])
     
     const submitHandler = (e) => {
@@ -39,26 +39,56 @@ const RecipeForm = () => {
                 <div>
                     <label>Title:</label>
                     <input type="text" value={title || ""} onChange={(e) => setTitle(e.target.value)}/>
+                    {
+                        errors.title?
+                        <p>{errors.title}</p>:
+                        null
+                    }
                 </div>
                 <div>
                     <label>Description:</label>
                     <input type="text" value={description || ""} onChange={(e) => setDescription(e.target.value)}/>
+                    {
+                        errors.description?
+                        <p>{errors.description}</p>:
+                        null
+                    }
                 </div>
                 <div>
                     <label>Ingredients:</label>
                     <input type="text" value={ingredients || ""} onChange={(e) => setIngredients(e.target.value)}/>
+                    {
+                        errors.ingredients?
+                        <p>{errors.ingredients}</p>:
+                        null
+                    }
                 </div>
                 <div>
                     <label>Directions:</label>
                     <input type="text" value={directions || ""} onChange={(e) => setDirections(e.target.value)}/>
+                    {
+                        errors.directions?
+                        <p>{errors.directions}</p>:
+                        null
+                    }
                 </div>
                 <div>
                     <label>Prep Time:</label>
                     <input type="number" value={prepTime || ""} onChange={(e) => setPrepTime(e.target.value)}/>
+                    {
+                        errors.prepTime?
+                        <p>{errors.prepTime}</p>:
+                        null
+                    }
                 </div>
                 <div>
                     <label>Cook Time:</label>
                     <input type="number" value={cookTime || ""} onChange={(e) => setCookTime(e.target.value)}/>
+                    {
+                        errors.cookTime?
+                        <p>{errors.cookTime}</p>:
+                        null
+                    }
                 </div>
                 <button>Create</button>
             </form>
