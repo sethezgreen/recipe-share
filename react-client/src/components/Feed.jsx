4856dc58d01cda3eab48d65498d6461be4a3da79
sethezgreen <sethezgreen@gmail.com>
2024-03-03 19:39:28 -0500

    updated Feed component

--- react-client/src/components/Feed.jsx
+++ react-client/src/components/Feed.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import '../css/feed.css'
 
-function Feed() {
+const Feed = (props) => {
+    const {token} = props
     const [recipes, setRecipes] = useState([])
 
     useEffect(() => {
-        axios.get('http://localhost:5000/api/recipes')
+        axios.get('http://localhost:5000/api/recipes', {headers: {Authorization: 'Bearer' + token}}) // authorization headers not working
             .then((res) => {
                 setRecipes(res.data)
                 // console.log(`response data: ${res.data[0]["title"]}`)
@@ -13,21 +15,30 @@ function Feed() {
                 console.log(res)
             })
             .catch(err => console.log(err))
-    }, [])
+    }, [token])
 
     return (
         <div className='feed'>
             {
                 recipes.map((recipe) => (
-                    <div key={recipe.id}>
-                        {/* <p>{recipe.title}</p>
-                        <p>{recipe.cook_time}</p> */}
-                        <p>{recipe}</p>
-                        <p>{recipe.title}</p>
+                    <div key={recipe.id} className='recipe-card'>
+                        <div className='top'>
+                            <p>{recipe.title}</p>
+                            <p>Posted By: {recipe.user.username}</p>
+                        </div>
+                        <div className='main'>
+                            <div>
+                                <p>Ingredients:</p>
+                                <p>{recipe.ingredients}</p>
+                            </div>
+                            <div>
+                                <p>Directions:</p>
+                                <p>{recipe.directions}</p>
+                            </div>
+                        </div>
                     </div>
                 ))
             }
-            <p>{recipes}</p>
         </div>
     )
 }
--- react-client/src/views/Main.jsx
+++ react-client/src/views/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './main.css'
 import TopNav from '../components/TopNav'
 import Feed from '../components/Feed'
+import { Link } from 'react-router-dom'
 
 const Main = (props) => {
     const {token} = props
@@ -10,7 +11,9 @@ const Main = (props) => {
         <div className='main_view'>
             <TopNav />
             {/* sidenav component */}
-            {/* <Feed /> */}
+            <h3>Feed</h3>
+            <Link to={'http://localhost:5173/create/recipe'}>add recipe</Link>
+            <Feed token={token}/>
             <p>Token from props: {token}</p>
         </div>
     )
