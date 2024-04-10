159e5b02b1ff6bce9e8b1d3b70c40e81dd237847
Seth Green <sethezgreen@gmail.com>
2024-04-10 10:26:43 -0400

    styling

--- react-client/src/components/ViewRecipe.jsx
+++ react-client/src/components/ViewRecipe.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import '../css/viewrecipe.css'
 import React, { useEffect, useState } from 'react'
 import UpdateRecipe from './UpdateRecipe'
 
@@ -41,24 +42,45 @@ const ViewRecipe = (props) => {
             {
                 editing? 
                     <UpdateRecipe recipeId={recipeId} setEditing={setEditing} token={token}/>:
-                    <div>
-                        <h1>{recipe.title}</h1>
-                        <h2>Posted By: {user.first_name} {user.last_name} <span className='blue-hover pointer-hover' onClick={(e) => usernameOnClick(e, recipe.user_id)}>(@{user.username})</span></h2>
-                        <p>Description: {recipe.description}</p>
-                        <p>Ingredients: {recipe.ingredients}</p>
-                        <p>Directions: {recipe.directions}</p>
-                        <p>Prep Time: {recipe.prep_time} min</p>
-                        <p>Cook Time: {recipe.cook_time} min</p>
-                        <p>Total Time: {recipe.prep_time + recipe.cook_time} min</p>
+                    <>
+                        <header className='recipe-header'>
+                            <h1 className='recipe-title'>{recipe.title}</h1>
+                            <p>{user.first_name} {user.last_name} <span className='blue-hover pointer-hover' onClick={(e) => usernameOnClick(e, recipe.user_id)}>(@{user.username})</span></p>
+                        </header>
+                        <div className='recipe-times'>
+                            <div className='time-section'>
+                                <p>Prep Time:</p>
+                                <p>{recipe.prep_time} min</p>
+                            </div>
+                            <div className='time-section'>
+                                <p>Cook Time:</p>
+                                <p>{recipe.cook_time} min</p>
+                            </div>
+                            <div className='time-section'>
+                                <p>Total Time:</p>
+                                <p>{recipe.prep_time + recipe.cook_time} min</p>
+                            </div>
+                        </div>
+                        <section>
+                            <p>{recipe.description}</p>                            
+                        </section>
+                        <section>
+                            <p className='sub-heading'>Ingredients</p>
+                            <p className='recipe-list'>{recipe.ingredients}</p>
+                        </section>
+                        <section>
+                            <p className='sub-heading'>Directions</p>
+                            <p className='recipe-list'>{recipe.directions}</p>
+                        </section>
                         {
                             tokenId == recipe.user_id?
-                            <div>
+                            <div className='recipe-btns'>
                                 <button onClick={() => setEditing(true)}>edit</button>
                                 <button onClick={() => deleteButtonOnClick()}>delete</button>
                             </div>:
                             null
                         }
-                    </div>
+                    </>
             }
         </div>
     )
