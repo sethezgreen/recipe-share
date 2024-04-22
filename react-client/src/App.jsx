7f5def1323356f3b6714ab262133fc8598c134fd
Seth Green <sethezgreen@gmail.com>
2024-04-22 10:23:27 -0400

    refactoring with routes

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router-dom'
 import { jwtDecode } from 'jwt-decode'
 import Main from './views/Main'
 import useToken from './components/useToken'
+import MainFeed from './components/MainFeed';
+import ViewUser from './components/ViewUser';
 
 function App() {
   const { token, setToken } = useToken("")
@@ -23,7 +25,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Main token={token} setToken={setToken} tokenId={tokenId} setTokenId={setTokenId} loggedUser={loggedUser} setLoggedUser={setLoggedUser} />} />
+        <Route exact path="/" element={<Main token={token} setToken={setToken} tokenId={tokenId} setTokenId={setTokenId} loggedUser={loggedUser} setLoggedUser={setLoggedUser} />}>
+          <Route path="feed" element={<MainFeed />} />
+          <Route path="user/:user_id" element={<ViewUser/>}/>
+        </Route>
       </Routes>
     </>
   )
--- react-client/src/views/Main.jsx
+++ react-client/src/views/Main.jsx
@@ -1,23 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import axios from 'axios'
 import '../css/main.css'
-import MainFeed from '../components/MainFeed'
-import ViewRecipe from '../components/ViewRecipe'
-import ViewUser from '../components/ViewUser'
 import SideNav from '../components/SideNav'
-import RecipeForm from '../components/RecipeForm'
 import LoginRegModal from '../components/LoginRegModal'
-import Bookmarks from '../components/Bookmarks'
 
 const Main = (props) => {
     const {token, setToken, tokenId, setTokenId, loggedUser, setLoggedUser} = props
-    const [recipeId, setRecipeId] = useState("")
-    const [userId, setUserId] = useState("")
     const [modal, setModal] = useState(false)
-    const [creating, setCreating] = useState(false)
     const [followedUsers, setFollowedUsers] = useState([])
-    const [viewingBookmarks, setViewingBookmarks] = useState(false)
-    const [bookmarks, setBookmarks] = useState([])
 
     useEffect(() => {
         if (token) {
@@ -43,8 +34,6 @@ const Main = (props) => {
     const logoutCallback = () => {
         setToken("")
         setTokenId("")
-        setRecipeId("")
-        setUserId("")
         setLoggedUser({})
         setFollowedUsers([])
         alert("logout successful")
@@ -54,51 +43,10 @@ const Main = (props) => {
         <div className='main-view'>
             <div id='border'></div>
             <div className='main-content'>
-            <SideNav token={token} toggleModal={toggleModal} logoutCallback={logoutCallback} loggedUser={loggedUser} setUserId={setUserId} followedUsers={followedUsers} setFollowedUsers={setFollowedUsers} setViewingBookmarks={setViewingBookmarks}/>
-            {
-                creating?
-                <div className='content-div'>
-                    <div className='content-header-container'>
-                        <button onClick={()=> setCreating(false)} className='content-header'>Back to Feed</button>
-                    </div>
-                    <RecipeForm token={token} setCreating={setCreating}/>
-                </div>:
-                recipeId?
-                <div className='content-div'>
-                    <div className='content-header-container'>
-                        <button onClick={()=> setRecipeId("")} className='content-header'>Back to Feed</button>
-                    </div>
-                    <ViewRecipe recipeId={recipeId} token={token} tokenId={tokenId} setRecipeId={setRecipeId} setUserId={setUserId}/>
-                </div>:
-                userId?
-                <div className='content-div'>
-                    {/* <div className='content-header-container'>
-                        
-                    </div> */}
-                    <ViewUser id={userId} setUserId={setUserId} setRecipeId={setRecipeId} tokenId={tokenId} token={token} followedUsers={followedUsers} setFollowedUsers={setFollowedUsers}/>
-                </div>:
-                viewingBookmarks?
-                <div className='content-div'>
-                    <div className='content-header-container'>
-                        <button onClick={()=> setViewingBookmarks(false)} className='content-header'>Back to Feed</button>
-                        <Bookmarks recipes={bookmarks}/>
-                    </div>
-                </div>:
-                <div className='content-div'>
-                    <div className='content-header-container'>
-                        <header className='content-header'>
-                            {/* <h3 className='background-border-rad primary'>Feed</h3> */}
-                            <h3 className=''>Feed</h3>
-                            {
-                                token?
-                                <button onClick={() => setCreating(true)} className='background-border-rad accent pointer-hover mobile-hidden'>post recipe</button>:
-                                <button onClick={() => toggleModal()} className='background-border-rad accent mobile-hidden'>Log in to post a recipe</button>
-                            }
-                        </header>
-                    </div>
-                    <MainFeed token={token} setRecipeId={setRecipeId} setUserId={setUserId}/>
-                </div>
-            }
+            <SideNav token={token} toggleModal={toggleModal} logoutCallback={logoutCallback} loggedUser={loggedUser}  followedUsers={followedUsers} setFollowedUsers={setFollowedUsers} />
+            <div className='content-div'>
+                <Outlet />
+            </div>
             </div>
 
             {
